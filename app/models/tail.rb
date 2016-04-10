65c6e03d2d6e288f88a4b231fba4bb387d729e00
strviola <the-brightest-moonlight.8107@mbp.nifty.com>
2016-04-10 16:43:02 +0900

    Fix post rule

--- app/models/tail.rb
+++ app/models/tail.rb
@@ -1,2 +1,5 @@
 class Tail < ActiveRecord::Base
+  validates :title, length: { maximum: 140 }, presence: true
+  validates :teller, length: { maximum: 70 }
+  validates :content, presence: true
 end
